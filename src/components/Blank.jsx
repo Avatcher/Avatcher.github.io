1d541ac303d6762df73e3b1e7ae2ccdd84f40e64
Avatcher <pravdindmytro@gmail.com>
2025-09-30 22:49:07 +0200

    Delete everything

--- src/components/Blank.jsx
+++
@@ -1,8 +0,0 @@
-export default function Blank(props) {
-    let style = {
-        height: props.size
-    }
-    return (
-        <div style={style}></div>
-    )
-}
--- src/pages/Infopage.jsx
+++
@@ -1,7 +0,0 @@
-import Layout from "../components/Layout";
-
-export default function Infopage() {
-    return (
-        <Layout></Layout>
-    )
-}
